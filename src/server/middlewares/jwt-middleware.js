871db34a6d9b2785bd594bd5b9c2b95adc0a6039
Gerard Banasig <gbanasig@ap.equinix.com>
2018-04-10 14:18:32 +0800

    Modify middelware jwt leave it blank for now

--- src/server/middlewares/jwt-middleware.js
+++ src/server/middlewares/jwt-middleware.js
@@ -1,13 +1,5 @@
 const jwtMiddleware = () => (req, res, next) => {
-  console.log('jwt-middleware invoked...');
-  const validToken = true;
-  const url = req.url;
-  console.log(`url : ${url}`);
-  if(url === '/reset-password'){
-
-  }
   next();
-  
 };
 
 export default jwtMiddleware;
